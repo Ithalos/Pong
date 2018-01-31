97a0e4209d5640d4c186dd40a5f237578621339e
Sam Triest <sam.triest@gmail.com>
2018-01-31 15:10:20 +0100

    Add Ball constructor

--- Ball.lua
+++ Ball.lua
@@ -18,3 +18,17 @@ Ball =
     dirY
 }
 
+--[[
+    Instantiate a new ball object and assign it a position and size.
+]]
+function Ball:Create(posX, posY, sizeX, sizeY)
+    newObject = {}
+    setmetatable(newObject, self)
+    self.__index = self
+    newObject.posX = posX
+    newObject.posY = posY
+    newObject.sizeX = sizeX
+    newObject.sizeY = sizeY
+    return newObject
+end
+
