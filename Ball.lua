8b7bcddb760db217e10c30e0a5e7f9e3f88a5f8f
Sam Triest <sam.triest@gmail.com>
2018-01-31 15:05:26 +0100

    Add Ball properties

---
+++ Ball.lua
@@ -0,0 +1,20 @@
+--[[
+    File: Ball.lua
+    Author: Sam Triest
+
+    The Ball class allows for the creation of the ball object
+    that the players must deflect.
+]]
+
+Ball =
+{
+    posX,
+    posY,
+
+    sizeX,
+    sizeY,
+
+    dirX,
+    dirY
+}
+
