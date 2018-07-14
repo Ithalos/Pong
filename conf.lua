d8284a7aff3e55e104a6fb2c01002d721e3f730d
Sam Triest <sam.triest@gmail.com>
2018-07-14 16:22:55 +0200

    Add version to conf.lua

--- conf.lua
+++ conf.lua
@@ -1,5 +1,7 @@
 -- Disable unnecessary modules to reduce memory usage
 function love.conf(t)
+    t.version = "11.0"
+
     t.modules.joystick = false
     t.modules.physics  = false
     t.modules.touch    = false
